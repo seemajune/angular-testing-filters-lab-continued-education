43ee694c0547409100faa8974e16c86f6b5f2474
Todd Motto <todd@toddmotto.com>
2016-02-22 16:22:46 +0000

    chore: Added solution

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,25 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+
+    it('should filter by gender', function () {
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+
+        $scope.vm.search = 'female';
+        $scope.vm.changeFilter();
+
+        expect($scope.vm.filteredList.length).toBe(71);
+    });
+
+    it('should filter by location', function () {
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+
+        $scope.vm.search = 'manchester';
+        $scope.vm.changeFilter();
+
+        expect($scope.vm.filteredList.length).toBe(8);
+    });
+
 });
